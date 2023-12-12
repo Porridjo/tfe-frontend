23c0f47374c88acd2b081c3c40c3b5f6ed16c0cf
KekManlol <kemanyinghi@outlook.fr>
2023-12-12 20:17:36 +0100

    Add routes for RoundPage, DeliverPage and DeliverDetails

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "../pages/Login/LoginPage";
+import RoundPage from "../pages/Round/RoundPage";
+import DeliverPage from "../pages/Deliver/DeliverPage";
+import DeliverDetails from "../pages/Deliver/DeliverDetails"
 import Home from "../pages/Home/Home";
 import { useState } from "react";
 
@@ -11,6 +14,9 @@ const App = () => {
     <Routes>
       <Route path="/" element={<LoginPage setUser={setUser} />} />
       <Route path="/home" element={user ? <Home /> : <Navigate replace to="/" />} />
+      <Route path='/round' element={<RoundPage />} />
+      <Route path='/round/:roundname' element={<DeliverPage />} />
+      <Route path='/round/:roundname/:nurseryname' element={<DeliverDetails />} />
     </Routes>
   );
 };
