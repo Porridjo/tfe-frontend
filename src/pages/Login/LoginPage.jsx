32de76f9bb782590543201e3798dd77e63843285
Vlaminck Maxime <maxime.vlaminck@outlook.com>
2023-12-14 17:54:06 +0100

    change: login request to axios

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,43 +1,46 @@
-import React, { useState } from 'react';
-import LoginForm from './LoginForm';
+import React, { useState } from "react";
+import LoginForm from "./LoginForm";
 import { useNavigate } from "react-router-dom";
-import '../../stylesheets/position.css'
+import axios from "axios";
+import "../../stylesheets/position.css";
 
-const LoginPage = ({setUser}) => {
-  const [messageLogin, setMessageLogin] = useState('');
+const LoginPage = ({ setUser }) => {
+  const [messageLogin, setMessageLogin] = useState("");
   const [isLogged, setIsLogged] = useState(undefined);
 
   const navigate = useNavigate();
 
-
   const handleLogin = async (credentials) => {
     try {
       // Effectuer l'appel à l'API pour le login
-      const response = await fetch('http://localhost:5000/users/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(credentials),
-      });
+      const response = await axios.post(
+        "http://localhost:5000/users/login",
+        credentials,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-      if (response.ok) {
+      if (response.status === 200) {
         // Authentification réussie
-        setMessageLogin('Authentification réussie!');
+        setMessageLogin("Authentification réussie!");
         setIsLogged(true);
-        const data = await response.json();
-        localStorage.setItem("user", JSON.stringify(data))
+        localStorage.setItem("user", JSON.stringify(response.data));
         setUser(credentials.username);
-        navigate('/round');
+        navigate("/round");
       } else {
         // Gérer les erreurs d'authentification
-        setMessageLogin('Veuillez entrer un identifiant et un mot de passe valide.');
+        setMessageLogin(
+          "Veuillez entrer un identifiant et un mot de passe valide."
+        );
         setIsLogged(false);
       }
     } catch (error) {
       // Gérer les erreurs liées à l'appel API
-      console.error('API call failed:', error);
-      setMessageLogin('Une erreur est survenue..');
+      console.error("API call failed:", error);
+      setMessageLogin("Une erreur est survenue..");
       setIsLogged(false);
     }
   };
@@ -46,9 +49,15 @@ const LoginPage = ({setUser}) => {
     <div className="center-container">
       <h1>Page de connexion</h1>
       <LoginForm onLogin={handleLogin} />
-      <p className= {isLogged ? 'validMessage' : isLogged == false ? 'errorMessage' : ''}>{messageLogin}</p>
+      <p
+        className={
+          isLogged ? "validMessage" : isLogged == false ? "errorMessage" : ""
+        }
+      >
+        {messageLogin}
+      </p>
     </div>
   );
-}
+};
 
 export default LoginPage;
